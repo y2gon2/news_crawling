8f622105e3a2de83419313e8bb05a246f2fcebdf
y2gon2 <y2gon2@gmail.com>
2023-01-25 10:05:42 +0900

    Added: chrome options

--- examples/ex5_chromium_test.py
+++ examples/ex5_chromium_test.py
@@ -1,4 +1,9 @@
 from selenium import webdriver
 
-driver = webdriver.Chrome('/usr/lib/chromium-browser/chromedriver')
+chrome_options = webdriver.ChromeOptions()
+chrome_options.add_argument('--headless')
+chrome_options.add_argument('--no-sandbox')
+chrome_options.add_argument('--disable-dev-shm-usage')
+
+driver = webdriver.Chrome('/usr/lib/chromium-browser/chromedriver', chrome_options=chrome_options)
 driver.get("http://www.google.com")
