991fae0099062cf1196ec75368e105203606cce6
y2gon2 <y2gon2@gmail.com>
2023-01-25 10:47:15 +0900

    Added: crawling PKG

---
+++ crawling/__init__.py
@@ -0,0 +1,10 @@
+from pymongo import MongoClient
+
+if __name__ == "__main__":
+    mongo_clust0 = 'mongodb+srv://gon:2025@cluster0.sm3ik1j.mongodb.net/?retryWrites=true&w=majority' # for using cloud
+    client = MongoClient(mongo_clust0)
+    db = client['myweb'].board
+
+    a = db.findAll()
+    print(a)
+
